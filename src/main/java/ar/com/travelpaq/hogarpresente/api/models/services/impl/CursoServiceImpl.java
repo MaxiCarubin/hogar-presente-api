d85e1560f58a3c6e6ebe7ee260a0a6e42dcbcd4d
Ixam <carubin.maximiliano@gmail.com>
2020-12-05 16:07:52 -0300

    se volvio a modificar el enpoint para testearlo

--- src/main/java/ar/com/travelpaq/hogarpresente/api/models/services/impl/CursoServiceImpl.java
+++ src/main/java/ar/com/travelpaq/hogarpresente/api/models/services/impl/CursoServiceImpl.java
@@ -118,19 +118,18 @@ public class CursoServiceImpl implements ICursoService {
                 return new ResponseEntity(new Mensaje("El precio no puede ser negativo"), HttpStatus.BAD_REQUEST);
             if(!usuarioRepository.existsById(cursoDto.getUsuarioId()))
                 return new ResponseEntity(new Mensaje("El usuario no esta en la base de datos"), HttpStatus.BAD_REQUEST);
-            UsuarioEntity usuarioEntity = usuarioRepository.findById(cursoDto.getUsuarioId()).get();
-            if(usuarioEntity.getRoles().contains(roleAlumno))
+            UsuarioEntity usuarioNuevo = usuarioRepository.findById(cursoDto.getUsuarioId()).get();
+            if(usuarioNuevo.getRoles().contains(roleAlumno))
                 return new ResponseEntity(new Mensaje("El usuario debe ser un capacitador o admin para editar el curso"), HttpStatus.BAD_REQUEST);
             CursoEntity cursoEntity = cursoRepository.getOne(id);
-            CursoEntity cursoEntityIf = cursoRepository.findById(id).get();
-            if (cursoDto.getUsuarioId() != cursoEntityIf.getUsuario().getId() || !cursoEntityIf.getUsuario().getRoles().contains(roleAdmin))
+            UsuarioEntity usuarioViejo = cursoRepository.findById(id).get().getUsuario();
+            if (usuarioNuevo.getId() != usuarioViejo.getId() || !usuarioViejo.getRoles().contains(roleAdmin))
                 return new ResponseEntity
                         (
-                                cursoEntityIf
-//                                ,new Mensaje
-//                                        (
-//                                                "El ID del creador del curso que se quiere modificar debe coincidir con el ID del usuario que esta modificando el curso o ese usuario debe ser ADMIN."
-//                                        )
+                                new Mensaje
+                                        (
+                                                "El ID del creador del curso que se quiere modificar debe coincidir con el ID del usuario que esta modificando el curso o ese usuario debe ser ADMIN."
+                                        )
                                 , HttpStatus.BAD_REQUEST
                         );
             cursoEntity.setNombre(cursoDto.getNombre());
